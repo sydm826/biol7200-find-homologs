03435bb015447a9f8e3c378bf81c1c7d0543e175
Sydney Moodie <smoodie319@gmail.com>
2026-09-21 05:52:28 -0400

    Add filtering by percent identity and match length, output match count

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,3 +5,7 @@ subject=$2
 output=$3
 
 tblastn -query "$query" -subject "$subject" -outfmt 6 > raw_blast_results.txt
+
+awk '$3 > 30 && $4 > 0.9 * $5' raw_blast_results.txt > "$output"
+
+wc -l < "$output"
